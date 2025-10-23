d14b4d6576cb262cecbe7dfc8d026f0f5d008ddd
flopius <flopodude@gmail.com>
2025-10-23 11:20:34 +0300

    Re-implemented the github icon to be outside of react

--- docs/_app/immutable/entry/start.ByeqdNhX.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/E83C-qGV.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.Cztf5jOZ.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/0W71CrRA.js";export{o as load_css,r as start};
